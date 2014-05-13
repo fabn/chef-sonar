dd1a3b2e81d1724244f193096d831fad96e5926b
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-05-13 20:09:21 +0200

    Readme does not exist

--- test/cookbooks/sonar_test/metadata.rb
+++ test/cookbooks/sonar_test/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Paul Dunnavant"
 maintainer_email "pdunnavant@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures sonar_test"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description "Simple test cookbook"
 version          "0.1.0"
 
 %w{ sonar openssl database nginx apache2 }.each do |cb|
